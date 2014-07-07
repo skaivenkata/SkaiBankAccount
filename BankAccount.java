10e017c4202a985a9cb550058f86dfd468f63127
skaivenkata <skai.venkata@gmail.com>
2014-07-07 04:50:45 -0400

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,20 @@
+package skai.com.vyella;
+
+public class BankAccount {
+
+  private double balance;
+
+  public BankAccount(double balance) {
+    this.balance = balance;
+  }
+
+  public double debit(double amount) {
+    if (balance < amount) {
+      amount = balance;
+    }
+
+    balance -= amount;
+    return amount;
+  }
+
+}
